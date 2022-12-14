cec89acb67b11fb7f90d2670dedf9340ae53756d
GentilSilva <silvagentil.sg@gmail.com>
2022-12-14 16:31:56 -0300

    início curso solid

---
+++ solidComJava/solid/src/br/com/alura/rh/Anotacoes.java
@@ -0,0 +1,63 @@
+package br.com.alura.rh;
+
+public class Anotacoes {
+
+	/*
+	 * Princípios da orientação a objetos
+	 */
+
+	/*
+	 * Coesão
+	 * 
+	 * Coesão -> "União Harmônica entre uma coisa e outra". No mundo da programação
+	 * é os atributos e métodos de uma classe trabalhar de forma harmônica, ou seja
+	 * não existe atributos ou métodos que distoam do que esta classe quer
+	 * representar.
+	 * 
+	 * "Classes não coesas tendem a crescer indefinidamente, o que as tornam difíceis de manter"
+	 * .
+	 * 
+	 */
+
+	/*
+	 * Encapsulamento
+	 *
+	 * Encapsulamento -> "Incluir ou proteger alguma coisa em uma cápsula". É você
+	 * proteger, blindar um atributo ou método contra manipulação externo a sua
+	 * classe.
+	 * 
+	 * "Classes não encapsulaas permitem violação de regras de negócio, além de aumentarem o acoplamento"
+	 * .
+	 * 
+	 */
+
+	/*
+	 * Acoplamento
+	 * 
+	 * Acoplamento -> "Ação de acoplar. Agrupamento aos pares". São dois componentes
+	 * (classes) interligados em que uma classe se comunica com a outra ou se
+	 * utiliza da outra.
+	 * 
+	 * "Classes acopladas causam fragilidade no código da aplicação, o que dificulta sua manutenção"
+	 * .
+	 * 
+	 */
+
+	/*
+	 * Coesão: 
+	 * 		Uma classe coesa faz bem uma única coisa 
+	 * 		Classes coesas não devem ter várias responsabilidades 
+	 * 
+	 * Encapsulamento: 
+	 * 		Getters e setters não são formas eficientes de aplicar encapsulamento 
+	 * 		É interessante fornecer acesso apenas ao que é necessário em nossas classes 
+	 * 		O encapsulamento torna o uso das nossasclasses mais fácil e intuitivo 
+	 * 
+	 * Acoplamento: 
+	 * 		Acoplamento é a dependência entre classes 
+	 * 		Acoplamento nem sempre é ruim, e que é impossível criar um sistema sem nenhum acoplamento 
+	 * 		Devemos controlar o nível de acoplamento na nossa aplicação
+	 * 
+	 */
+
+}
