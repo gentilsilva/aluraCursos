71e275834c34c6971574ea2c3426e21721df7a3c
Gentil SIlva <silvagentil.sg@gmail.com>
2023-01-12 02:57:20 -0300

    Término curso spring boot 3 I

---
+++ formacoes/javaSpringBoot/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,61 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        medicoRepository.save(new Medico(dados));
+    }
+
+//	@GetMapping
+//	public List<DadosListagemMedico> listar() {
+//		return medicoRepository.findAll().stream().map(DadosListagemMedico::new).toList();
+//	}
+
+//	@GetMapping
+//	public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+//		return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+//	}
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        Medico medico = medicoRepository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
+    }
+
+//	Exclusão física, retira os dados do banco de dados
+//	@DeleteMapping("/{id}")
+//	@Transactional
+//	public void excluir(@PathVariable Long id) {
+//		medicoRepository.deleteById(id);
+//	}
+
+    @DeleteMapping("{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        Medico medico = medicoRepository.getReferenceById(id);
+        medico.inativar();
+    }
+
+}
---
+++ formacoes/javaSpringBoot/api/src/main/resources/db/migration/V5__alter-table-pacientes-add-column-ativo.sql
@@ -0,0 +1,3 @@
+ALTER TABLE pacientes ADD COLUMN ativo TINYINT;
+UPDATE pacientes SET ativo = 1;
+ALTER TABLE pacientes MODIFY ativo TINYINT NOT NULL;
